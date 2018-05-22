8caf0135c152f3825c3bd0917dd8feddbeaa1a65
cuijianjun <376247934@qq.com>
2018-05-22 17:25:53 +0800

    message-weixin

--- wechat/pages/message/index.js
+++ wechat/pages/message/index.js
@@ -5,7 +5,18 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    items: [
+      { avatar: 'avatar_0.jpg', name: '刘备', recent: '这一拜，生死不改', time: '刚刚' },
+      { avatar: 'avatar_1.jpg', name: '关羽', recent: '谁赶在我面前耍大刀', time: '一分钟前' },
+      { avatar: 'avatar_3.jpg', name: '张飞', recent: '二哥不着急，让俺来收拾他', time: '2018-4-20' },
+      { avatar: 'avatar_4.jpg', name: '小乔', recent: '男人总是打打杀杀的，一点都不好玩', time: '2018-4-20' },
+      { avatar: 'avatar_5.jpg', name: '周瑜', recent: '小乔的那个头像让我很纠结！', time: '2018-4-20' },
+      { avatar: 'avatar_3.jpg', name: '曹操', recent: '你们在赤壁把我坑惨了，你们等着', time: '2018-4-20' },
+      { avatar: 'avatar_7.jpg', name: '诸葛亮', recent: '曹操，你打我啊，打我啊，我也不怕你', time: '2018-4-20' },
+      { avatar: 'avatar_8.jpg', name: '王朗', recent: '诸葛村夫，咱们再战三百回合！', time: '2018-4-20' },
+      { avatar: 'avatar_9.jpg', name: '司马懿', recent: '撤回了一条消息', time: '2018-4-20' },
+      { avatar: 'avatar_1.jpg', name: '赵云', recent: '贵圈真乱，我要回家', time: '2018-4-20' }
+    ]
   },
 
   /**
