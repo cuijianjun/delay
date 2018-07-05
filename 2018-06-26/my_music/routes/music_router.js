71e4d66bc93b435fbc67061cf9e5e7efd8303447
cuijianjun <376247934@qq.com>
2018-07-06 00:28:32 +0800

    test

--- 2018-06-26/my_music/routes/music_router.js
+++
@@ -1,9 +0,0 @@
-const Router = require('koa-router');
-let router = new Router();
-let musicController = require('../controllers/music_controller')
-
-//音乐路由
-router.get('/music/index',musicController.showIndex)//首页
-.post('/music/add-music',musicController.addMusics) //上传音乐
-
-module.exports = router
